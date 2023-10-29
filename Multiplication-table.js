6a94be4853d103edafaffc516c2beb610536bb77
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-28 21:57:37 -0700

    Update Multiplication-table.js

--- Multiplication-table.js
+++ Multiplication-table.js
@@ -25,3 +25,14 @@ multiplicationTable = function(size) {
 }
 
 // or
+
+const multiplicationTableTwo = n => {
+  const res = [];
+  for (let i = 1; i <= n; i++) {
+    const row = [];
+    for (let j = 1; j <= n; j++)
+      row.push(i * j);
+    res.push(row);
+  }
+  return res;
+}
