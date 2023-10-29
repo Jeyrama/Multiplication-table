7e701d65bd4077bdf003f351f024e64d7460e4aa
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-28 21:54:36 -0700

    Update Multiplication-table.js

--- Multiplication-table.js
+++ Multiplication-table.js
@@ -11,3 +11,15 @@ For the given example, the return value should be:
 
 
 // Solution
+
+multiplicationTable = function(size) {
+  let result = [];
+
+  for (let i = 0; i < size; i++) {
+    result[i] = [];
+    for(let j = 0; j < size; j++) {
+      result[i][j] = (i + 1) * (j + 1);
+    }
+  }
+  return result
+}
